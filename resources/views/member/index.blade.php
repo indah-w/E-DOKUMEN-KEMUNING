531f994ab5d4cd04d4b08bd9e93276167c043b63
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2020-08-26 14:34:04 +0700

    add btn

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -73,6 +73,10 @@
 <script>
     $(function () {
       $("#example1").DataTable({
+      	dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
         "columnDefs": [
             { "width": "10%", "targets": -1 }
         ]
